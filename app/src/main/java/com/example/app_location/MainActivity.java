4b0e67719daa28201804284525053839fee94395
MARYAME <Lenovo@MARYAME-EL0MARI>
2024-04-10 13:55:21 +0000

    first-interface

---
+++ app/src/main/java/com/example/app_location/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.app_location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText EmailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
