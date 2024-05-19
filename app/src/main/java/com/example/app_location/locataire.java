dd9bb37762fbc364fd5f55039fa1c2deddcefe27
MARYAME <Lenovo@MARYAME-EL0MARI>
2024-05-19 14:00:26 +0100

    stockage donnee

--- app/src/main/java/com/example/app_location/locataire.java
+++ app/src/main/java/com/example/app_location/locataire.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -22,15 +23,21 @@ import java.util.ArrayList;
 
 public class locataire  extends AppCompatActivity {
     private Button suivantButton,RetourButton;
-    private Spinner Ville_spinner,Quartier_spinner;
+    private ImageButton backButton;
+    private Spinner Ville_spinner,Quartier_spinner,prix_spinner,Type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.locataire);
         suivantButton = findViewById(R.id.suivantButton);
         RetourButton=findViewById(R.id.RetourButton);
+        backButton= findViewById(R.id.backButton);
         Ville_spinner=findViewById(R.id.Ville_spinner);
         Quartier_spinner=findViewById(R.id.Quartier_spinner);
+        prix_spinner=findViewById(R.id.prix_spinner);
+        Type=findViewById(R.id.Type);
+
+
         suivantButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -42,15 +49,27 @@ public class locataire  extends AppCompatActivity {
         RetourButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(locataire.this, liste_propriete.class);
+                startActivity(intent);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(locataire.this, QuiEtes.class);
                 startActivity(intent);
             }
         });
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         CollectionReference citiesRef = db.collection("Ville");
+        CollectionReference priceRef = db.collection("Prix");
+
         ArrayList<String> villes = new ArrayList<String>();
+        ArrayList<String> prix = new ArrayList<String>();
+
 
         citiesRef.get()
+
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -58,11 +77,54 @@ public class locataire  extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d("TAG", document.getId() + " => " + document.getData());
                                 villes.add(document.getId());
+
                             }
                             // Create adapter and set it to the spinner here
                             ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(locataire.this, android.R.layout.simple_spinner_item, villes.toArray(new String[villes.size()]));
                             dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                             Ville_spinner.setAdapter(dataAdapter);
+
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+
+                        priceRef.get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                                prix.add(document.getId());
+                                            }
+                                            // Create adapter and set it to the spinner here
+                                            ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>(locataire.this, android.R.layout.simple_spinner_item, prix.toArray(new String[prix.size()]));
+                                            dataAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                            prix_spinner.setAdapter(dataAdapter1);
+                                        } else {
+                                            Log.w("TAG", "Error getting documents.", task.getException());
+                                        }
+                                    }
+                                });
+                    }
+                });
+
+        CollectionReference typeRef = db.collection("Type_logement");
+        ArrayList<String> type = new ArrayList<String>();
+
+        typeRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                type.add(document.getId());
+                            }
+                            // Create adapter and set it to the spinner here
+                            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(locataire.this, android.R.layout.simple_spinner_item, type.toArray(new String[type.size()]));
+                            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            Type.setAdapter(dataAdapter);
                         } else {
                             Log.w("TAG", "Error getting documents.", task.getException());
                         }
--- app/src/main/java/com/example/app_location/proprietaire.java
+++ app/src/main/java/com/example/app_location/proprietaire.java
@@ -1,43 +1,170 @@
 package com.example.app_location;
 
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class proprietaire extends AppCompatActivity {
-    private Button suivantButton, RetourButton;
-    private final int MENU_PROFIL = R.id.profilmenu;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class proprietaire extends AppCompatActivity {
+
+    private EditText photos,description,contacte,tarif;
+    private Button LancerButton, AnnulerButton;
+    private ImageButton backButton ,imageViewSecondary,imageViewMain;
+    private final int MENU_PROFIL = R.id.profilmenu;
+    private Spinner Ville_spinner,Type;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.proprietaire);
 
-        suivantButton = findViewById(R.id.suivantButton);
-        RetourButton = findViewById(R.id.RetourButton);
+        LancerButton = findViewById(R.id.LancerButton);
+        AnnulerButton = findViewById(R.id.AnnulerButton);
+        backButton = findViewById(R.id.backButton);
+        photos = findViewById(R.id.photos);
+        description = findViewById(R.id.description);
+        contacte = findViewById(R.id.contacte);
+        tarif = findViewById(R.id.tarif);
 
-        suivantButton.setOnClickListener(new View.OnClickListener() {
+        Type=findViewById(R.id.Type);
+
+
+        Ville_spinner = findViewById(R.id.Ville_spinner);
+
+// quand je clique sur lancer les donnees saisi seront enregistré
+        LancerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(proprietaire.this, liste_propriete.class);
+                saveDataToFirestore();
+
+            }
+        });
+        AnnulerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(proprietaire.this, tableau_bord.class);
                 startActivity(intent);
             }
         });
-        RetourButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(proprietaire.this, QuiEtes.class);
                 startActivity(intent);
             }
         });
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference citiesRef = db.collection("Ville");
+        ArrayList<String> villes = new ArrayList<String>();
+
+        citiesRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                villes.add(document.getId());
+                            }
+                            // Create adapter and set it to the spinner here
+                            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(proprietaire.this, android.R.layout.simple_spinner_item, villes.toArray(new String[villes.size()]));
+                            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            Ville_spinner.setAdapter(dataAdapter);
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+        CollectionReference typeRef = db.collection("Type_logement");
+        ArrayList<String> type = new ArrayList<String>();
+
+        typeRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                type.add(document.getId());
+                            }
+                            // Create adapter and set it to the spinner here
+                            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(proprietaire.this, android.R.layout.simple_spinner_item, type.toArray(new String[type.size()]));
+                            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            Type.setAdapter(dataAdapter);
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
+
+        private void saveDataToFirestore() {
+            String photo = photos.getText().toString();
+            String descriptionText = description.getText().toString();
+            String contact = contacte.getText().toString();
+            String tarifText = tarif.getText().toString();
+            String ville = Ville_spinner.getSelectedItem().toString();
+            String types = Type.getSelectedItem().toString();
+
+
+            if (photo.isEmpty() || descriptionText.isEmpty() || contact.isEmpty() || tarifText.isEmpty() || ville.isEmpty()) {
+                Toast.makeText(this, "Veuillez remplir tous les champs.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Map<String, Object> userData = new HashMap<>();
+            userData.put("photo", photo);
+            userData.put("description", descriptionText);
+            userData.put("contact", contact);
+            userData.put("tarif", tarifText);
+            userData.put("ville", ville);
+            userData.put("type", types);
+
+            db.collection("users")
+                    .add(userData)
+                    .addOnSuccessListener(documentReference -> {
+                        Toast.makeText(proprietaire.this, "Les données ont été Lancées.", Toast.LENGTH_SHORT).show();
+                        // Rediriger vers la page suivante après l'enregistrement des données
+                        Intent intent = new Intent(proprietaire.this, liste_propriete.class);
+                        startActivity(intent);
+                    })
+                    .addOnFailureListener(e -> Toast.makeText(proprietaire.this, "Erreur lors de l'enregistrement des données.", Toast.LENGTH_SHORT).show());
+        }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
